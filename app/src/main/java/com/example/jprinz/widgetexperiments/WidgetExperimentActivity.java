6ff560fb0582f1c3d08dbc9e288ddc683de8ded2
jprinz <jprinz@conestogac.on.ca>
2017-05-10 10:42:27 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jprinz/widgetexperiments/WidgetExperimentActivity.java
@@ -0,0 +1,13 @@
+package com.example.jprinz.widgetexperiments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WidgetExperimentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widget_experiment);
+    }
+}
